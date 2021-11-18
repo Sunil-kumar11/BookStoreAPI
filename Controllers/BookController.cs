ed41e5f90df3efdfe4d9936a6ea4ca7c193ff66d
Sunil-kumar11 <jaysanny@gmail.com>
2021-11-18 18:32:49 +0530

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,47 @@
+﻿using BookStoreAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public List<Book> books = new List<Book>()
+        {
+            new Book
+            {
+                Id=001,Title="C#",Author="Anders Hejlsberg",PublicationYear=2000,IsAvailable=true,CallNumber="+919912345678"
+            },new Book
+            {
+                Id=002,Title="Java",Author="James Gosling",PublicationYear=1996,IsAvailable=true,CallNumber="+919912345678"
+            },new Book
+            {
+                Id=003,Title="C++",Author="Bjarne Stroustrup",PublicationYear=1985,IsAvailable=true,CallNumber="+919912345678"
+            },new Book
+            {
+                Id=004,Title="Python",Author="Guido van Rossum",PublicationYear=1994,IsAvailable=true,CallNumber="+919912345678"
+            },new Book
+            {
+                Id=005,Title="javascript",Author="Brendan Eich",PublicationYear=1995,IsAvailable=true,CallNumber="+919912345678"
+            },new Book
+            {
+                Id=006,Title="PhP",Author="Rasmus Lerdorf",PublicationYear=1994,IsAvailable=true,CallNumber="+919912345678"
+            }
+        };
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>>  GetAllBooks()
+        {
+            return books;
+        }
+
+
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> get()
+        {
+            return new string[] { "values 1", "values 2" ,"values 3","values 4" };
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "the string is " + id;
+        }
+
+    }
+}
